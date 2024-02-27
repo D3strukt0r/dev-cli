4ab70f9e101604de4b91f5a53ae2edb2f21a10d6
D3strukt0r <dev@d3strukt0r.me>
2024-02-27 08:26:26 +0100

    Document new commands

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,29 @@ mod utils;
 
 use clap::Parser;
 
+// Parameters for config
+// - docker-compose-path: Path to the docker-compose file (default: {project-root}/compose.yml)
+// - build-script-path: Path to the build script (ex.: ./docker/build/build.sh)
+// - run-commands: List of commands to run in the container (ex.: [
+//    "dev": {commands: [{container: "node", user: "node", command: "yarn run dev"}]},
+//    "update": {
+//      parallel: true,
+//      commands: [
+//        {container: "node", user: "node", command: "yarn upgrade"},
+//        {container: "php", user: "www-data", command: "composer update"},
+//    },
+//    "clear-cache": {commands: [{container: "php", user: "www-data, command: "rm -rf var/cache/*"}]},
+// ])
+
 #[derive(Debug, clap::Parser)]
 #[command(version, about = "A CLI for managing local Docker development environments", long_about = None)]
 struct Cli {
     /// The name of the service to run the command in. If omitted, the first service in the project will be used.
     #[arg(short, long)]
     service: Option<String>,
+
+    /// Run the command in offline mode. This will prevent dev-cli from trying to connect to the internet.
+    offline: Option<bool>,
 
     #[command(subcommand)]
     command: Option<Commands>,
@@ -21,6 +38,43 @@ struct Cli {
 
 #[derive(Debug, clap::Subcommand, PartialEq)]
 pub enum Commands {
+    /// Initialize a new project for dev-cli using pre-defined templates
+    Init,
+    /// Starts a docker compose project
+    Start,
+    /// Stop and remove the containers of a project. Does not lose or harm anything unless you add --remove-data.
+    Stop {
+        #[arg(long, default_value("false"))]
+        remove_data: bool,
+    },
+    /// Stops, removes and starts a project again
+    Restart,
+    /// Stop all projects and dev-cli containers (Traefik, etc.)
+    Poweroff,
+    /// Execute a shell command in the container for a service.
+    Exec {
+        #[arg(short, long)]
+        service: Option<String>,
+
+        #[arg(short, long)]
+        user: Option<String>,
+
+        command: Vec<String>,
+    },
+    /// Run a command defined in the config file
+    Run {
+        command: Vec<String>,
+    },
+    /// Starts a shell session in the container for a service
+    Shell,
+    /// Launches the default URL in the default browser
+    Launch,
+    /// Show the status of the containers of this project
+    Status,
+    /// Show the status of all projects that ran through dev-cli
+    GlobalStatus,
+
+
     // Removes items dev-cli has created
     //Clean,
     // Generate the autocompletion script for the specified shell
@@ -31,13 +85,6 @@ pub enum Commands {
     //Delete,
     // Get a detailed description of a running dev-cli project
     //Describe,
-    /// Execute a shell command in the container for a service.
-    Exec {
-        #[arg(short, long)]
-        service: Option<String>,
-
-        command: Vec<String>,
-    },
     // Dump a database to a file or to stdout
     //ExportDb,
     // Get/Download a 3rd party add-on (service, provider, etc.)
@@ -52,23 +99,10 @@ pub enum Commands {
     //List,
     // Get the logs from your running services.
     //Logs,
-    // Completely stop all projects and containers
-    //Poweroff,
-    // Restart a project or several projects.
-    //Restart,
     // Add or remove, enable or disable extra services
     //Service,
     // Create a database snapshot for one or more projects.
     //Snapshot,
-    // Starts a shell session in the container for a service
-    //Ssh,
-    /// Starts a dev-cli project
-    Start,
-    /// Stop and remove the containers of a project. Does not lose or harm anything unless you add --remove-data.
-    Stop {
-        #[arg(long, default_value("false"))]
-        remove_data: bool,
-    },
 }
 
 // Global constants for config file names
